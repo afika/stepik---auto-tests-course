640ec5bcc68038b94aac34c647eaf89f2625bc38
afika <nastya.tk6990@gmail.com>
2019-07-07 21:35:05 +0700

    add lesson1.5_step9 homework

---
+++ lesson1.5_step9.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+import time
+
+link = "http://suninjuly.github.io/registration1.html"
+browser = webdriver.Chrome()
+browser.get(link)
+
+# Ваш код, который заполняет обязательные поля
+browser.find_element_by_css_selector("input[required][class='form-control first']").send_keys("Ivan")
+
+browser.find_element_by_css_selector("input[required][class='form-control second']").send_keys("Petrov")
+
+browser.find_element_by_css_selector("input[required][class='form-control third']").send_keys("name@email.domain")
+
+
+# Отправляем заполненную форму
+button = browser.find_element_by_css_selector("button.btn")
+button.click()
+
+# Проверяем, что смогли зарегистрироваться
+# ждем загрузки страницы
+time.sleep(1)
+
+# находим элемент, содержащий текст
+welcome_text_elt = browser.find_element_by_tag_name("h1")
+# записываем в переменную welcome_text текст из элемента welcome_text_elt
+welcome_text = welcome_text_elt.text
+
+# с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+assert "Поздравляем! Вы успешно зарегистировались!" == welcome_text
+browser.quit()
